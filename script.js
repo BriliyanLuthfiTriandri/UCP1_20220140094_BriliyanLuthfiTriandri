0f0d586a9e148c2fc9fd583d53982ce976155852
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-05-07 01:33:51 +0700

    codingan js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showAlert(){
+    alert(Date(),show())
+
+    let tanggal = new Date();
+    Document.getElementById("Hasil").innerHTML - tanggal;
+
+    function show(){
+        alert("Perkenalkan, aku Gordon. Aku adalah salah satu kereta senior tercepat dan terkuat berwarna biru. Selain itu, aku merupakan sosok yang pemaaf dan senang membantu kereta kecil ketika sedang dalam masalah.");
+    }
+}
+
+    
